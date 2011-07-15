1a90469311c1e8e8ceafbc42a731d4e351ad1f95
Edward Kaplan <ted@philip-kaplans-imac.local>
2011-07-15 17:53:00 -0400

    completed code to do searches, refactored a bit

--- megabus-routes.py
+++ megabus-routes.py
@@ -4,109 +4,135 @@
 # AJAX updates to destinations select box 
 # john@lawnjam.com
 
+import datetime
 from BeautifulSoup import BeautifulSoup
 import urllib, urllib2, cookielib
 
-cj = cookielib.CookieJar()
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-urllib2.install_opener(opener)
+START_URL = 'http://us.megabus.com/default.aspx'
+RESULTS_URL = 'http://us.megabus.com/JourneyResults.aspx'
+HEADERS = [('User-Agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3'),
+        ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'),
+        ('Accept-Language', 'en-gb,en;q=0.8,en-us;q=0.5,gd;q=0.3'),
+        ('Accept-Encoding', 'gzip,deflate'),
+        ('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7),*;q=0.7'),]
 
-headers = {'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en-gb,en;q=0.8,en-us;q=0.5,gd;q=0.3',
-    'Accept-Encoding': 'gzip,deflate',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7'}
+def init_page(headers=HEADERS):
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    opener.addheaders = headers
+    response = opener.open(START_URL, None)
 
-req = urllib2.Request('http://uk.megabus.com/default.aspx', None, headers)
-response = urllib2.urlopen(req)
-
-megaSoup = BeautifulSoup(response.read())
-viewstate = megaSoup.find(name='input', attrs={'name': '__VIEWSTATE'})['value']
-eventvalidation = megaSoup.find(name='input', attrs={'name': '__EVENTVALIDATION'})['value']
-options = megaSoup.find(name='select', attrs={'name': 'SearchAndBuy1$ddlLeavingFrom'}).findAll('option')
-startLocations = {}
-for o in options:
-    startLocations[int(o['value'])] = o.find(text=True)
-del startLocations[0] # 0 is "Select"
-
-# set other form values
-values = {
-    'Welcome1_ScriptManager1_HiddenField': '',
-    'Welcome1$ScriptManager1': 'SearchAndBuy1$upSearchAndBuy|SearchAndBuy1$ddlLeavingFrom',
-    '__EVENTTARGET': 'SearchAndBuy1$ddlLeavingFrom',
-    '__EVENTARGUMENT': '',
-    'Welcome1$hdnBasketItemCount': '0',
-    'Language1$ddlLanguage': 'en',
-    'SearchAndBuy1$txtPassengers': '1',
-    'SearchAndBuy1$txtConcessions': '0',
-    'SearchAndBuy1$txtNUSExtra': '0',
-    'SearchAndBuy1$txtOutboundDate': '',
-    'SearchAndBuy1$txtReturnDate': '',
-    'SearchAndBuy1$txtPromotionalCode': '',
-    '__ASYNCPOST': 'true'
-    }
-headers['X-MicrosoftAjax'] = 'Delta=true'
-
-for a in startLocations:
-    values['SearchAndBuy1$ddlLeavingFrom'] = a
+    megaSoup = BeautifulSoup(response.read())
+    viewstate = megaSoup.find(name='input', attrs={'name': '__VIEWSTATE'})['value']
+    eventvalidation = megaSoup.find(name='input', attrs={'name': '__EVENTVALIDATION'})['value']
+    options = megaSoup.find(name='select', attrs={'name': 'SearchAndBuy1$ddlLeavingFrom'}).findAll('option')
+    startLocations = {}
+    for o in options:
+        startLocations[int(o['value'])] = o.find(text=True)
+    del startLocations[0] # 0 is "Select"
+    values = {
+        'Welcome1_ScriptManager1_HiddenField': '',
+        'Welcome1$ScriptManager1': 'SearchAndBuy1$upSearchAndBuy|SearchAndBuy1$ddlLeavingFrom',
+        '__EVENTTARGET': 'SearchAndBuy1$ddlLeavingFrom',
+        '__EVENTARGUMENT': '',
+        'Welcome1$hdnBasketItemCount': '0',
+        'Language1$ddlLanguage': 'en',
+        'SearchAndBuy1$txtPassengers': '1',
+        'SearchAndBuy1$txtConcessions': '0',
+        'SearchAndBuy1$txtNUSExtra': '0',
+        'SearchAndBuy1$txtOutboundDate': '',
+        'SearchAndBuy1$txtReturnDate': '',
+        'SearchAndBuy1$txtPromotionalCode': '',
+        '__ASYNCPOST': 'true'
+        }
+    opener.addheaders.append(('X-MicrosoftAjax', 'Delta=true'))
     values['__EVENTVALIDATION'] = eventvalidation
     values['__VIEWSTATE'] = viewstate
+    return startLocations, (opener, values)
+
+
+def ajax_to_dict(piped_str):
+    L = piped_str.split('|')
+    return dict(zip(L[::2],L[1::2]))
+
+# set other form values
+
+def set_start(start, (opener, values)):
+    values['SearchAndBuy1$ddlLeavingFrom'] = start
     data = urllib.urlencode(values)
-    req = urllib2.Request('http://uk.megabus.com/default.aspx', data, headers)
+    response = opener.open(START_URL, data)
 
     # store the received (pipe-separated) data in a list
-    L = urllib2.urlopen(req).read().split('|')
-    for position, item in enumerate(L):
-        if item == 'SearchAndBuy1_upSearchAndBuy':
-            html = L[position + 1]
-        if item == '__VIEWSTATE':
-            viewstate = L[position + 1] # save __VIEWSTATE for the next iteration
-        if item == '__EVENTVALIDATION':
-            eventvalidation = L[position + 1] # save __EVENTVALIDATION for the next iteration
+    resp_dict = ajax_to_dict(response.read())
+    html = resp_dict['SearchAndBuy1_upSearchAndBuy']
+    # save ASP.NET state junk
+    viewstate = resp_dict['__VIEWSTATE']
+    eventvalidation = resp_dict['__EVENTVALIDATION']
 
     megaSoup = BeautifulSoup(html)
     options = megaSoup.find(name='select', attrs={'name': 'SearchAndBuy1$ddlTravellingTo'}).findAll('option')
     endLocations = {}
     for o in options:
         if int(o['value']) > 0:
-            print '"' + startLocations[a] + '","' + o.find(text=True) + '"'
-            #endLocations[int(o['value'])] = o.find(text=True)
-    #print endLocations
+            #print '"' + startLocations[a] + '","' + o.find(text=True) + '"'
+            endLocations[int(o['value'])] = o.find(text=True)
+    values['__EVENTVALIDATION'] = eventvalidation
+    values['__VIEWSTATE'] = viewstate
+    return endLocations
 
-"""
 #print endLocations
 
 # 2nd POST: set travelling to
-values['__EVENTVALIDATION'] = eventvalidation
-values['__VIEWSTATE'] = viewstate
-values['Welcome1$ScriptManager1'] = 'SearchAndBuy1$upSearchAndBuy|SearchAndBuy1$ddlTravellingTo'
-values['__EVENTTARGET'] = 'SearchAndBuy1$ddlTravellingTo'
-values['__LASTFOCUS'] = ''
-values['SearchAndBuy1$ddlTravellingTo'] = '10' # 10 is Birmingham
-data = urllib.urlencode(values)
-req = urllib2.Request('http://uk.megabus.com/default.aspx', data, headers)
+def set_dest(dest, (opener, values)):
+    values['Welcome1$ScriptManager1'] = 'SearchAndBuy1$upSearchAndBuy|SearchAndBuy1$ddlTravellingTo'
+    values['__EVENTTARGET'] = 'SearchAndBuy1$ddlTravellingTo'
+    values['__LASTFOCUS'] = ''
+    values['SearchAndBuy1$ddlTravellingTo'] = dest
+    data = urllib.urlencode(values)
+    response = opener.open(START_URL, data)
 
-# store the received (pipe-separated) data in a list
-L = urllib2.urlopen(req).read().split('|')
+    # store the received (pipe-separated) data in a list
+    resp_dict = ajax_to_dict(response.read())
+    html = resp_dict['SearchAndBuy1_upSearchAndBuy']
+    # save ASP.NET state junk
+    viewstate = resp_dict['__VIEWSTATE']
+    eventvalidation = resp_dict['__EVENTVALIDATION']
+    values['__VIEWSTATE'] = viewstate
+    values['__EVENTVALIDATION'] = eventvalidation
+    return (opener, values)
 
-for position, item in enumerate(L):
-    if item == '__VIEWSTATE':
-        viewstate = L[position + 1]
-    if item == '__EVENTVALIDATION':
-        eventvalidation = L[position + 1]
+def set_outbound_date(date, (opener, values)):
+    # 3rd POST: set date
+    date_ref = datetime.datetime(2000, 1, 1)
+    date_offset = (date - date_ref).days
+    values['Welcome1$ScriptManager1'] = 'SearchAndBuy1$upOutboundDate|SearchAndBuy1$calendarOutboundDate'
+    values['__EVENTTARGET'] = 'SearchAndBuy1$calendarOutboundDate'
+    values['__EVENTARGUMENT'] = date_offset
+    data = urllib.urlencode(values)
+    response = opener.open(START_URL, data)
+    resp_dict = ajax_to_dict(response.read())
+    print resp_dict
+    html = resp_dict['SearchAndBuy1_upSearchAndBuy']
+    # save ASP.NET state junk
+    viewstate = resp_dict['__VIEWSTATE']
+    eventvalidation = resp_dict['__EVENTVALIDATION']
+    values['__VIEWSTATE'] = viewstate
+    values['__EVENTVALIDATION'] = eventvalidation
 
-# 3rd POST: set date
-values['__EVENTVALIDATION'] = eventvalidation
-values['__VIEWSTATE'] = viewstate
-values['Welcome1$ScriptManager1'] = 'SearchAndBuy1$upSearchAndBuy|SearchAndBuy1$calendarOutboundDate'
-values['__EVENTTARGET'] = 'SearchAndBuy1$calendarOutboundDate'
-values['__EVENTARGUMENT'] = '4087' ###### FIXME map these values to actual dates - 4087 is 11/03/2011
-values['SearchAndBuy1$ddlTravellingBy'] = '0'
-data = urllib.urlencode(values)
-req = urllib2.Request('http://uk.megabus.com/default.aspx', data, headers)
-urllib2.urlopen(req)
+def do_search((opener, values)):
+    values['Welcome1$ScriptManager1'] = 'SearchAndBuy1$upSearchAndBuy|SearchAndBuy1$btnSearch'
+    values['__EVENTTARGET'] = ''
+    values['__EVENTARGUMENT'] = ''
+    data = urllib.urlencode(values)
+    response = opener.open(START_URL, data)
+    resp_dict = ajax_to_dict(response.read())
+    viewstate = resp_dict['__VIEWSTATE']
+    eventvalidation = resp_dict['__EVENTVALIDATION']
+    values['__VIEWSTATE'] = viewstate
+    values['__EVENTVALIDATION'] = eventvalidation
+    pass
 
+def get_results((opener, values)):
 # GET the results
-req = urllib2.Request('http://uk.megabus.com/JourneyResults.aspx', None, headers)
-print urllib2.urlopen(req).read()
-"""
+    response = opener.open(RESULTS_URL, None)
+    return response.read()
