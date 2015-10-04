9d1ee263fd21499c7f6bb86e790157c4e06b0888
Alice <aliceren@seas.upenn.edu>
2015-10-03 22:59:52 -0400

    command-line interface for fetching boltbus fares

--- bolt-runner.py
+++ bolt-runner.py
@@ -3,24 +3,50 @@ import random
 import datetime
 import json
 
+import argparse
+import dateutil.parser as dparser
 
-bus = BoltbusScraper()
+Boltbus = BoltbusScraper()
 
-leaving = datetime.date.today() + datetime.timedelta(days=1)
-print leaving.strftime("%m/%d/%Y")
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-d', '--date', help='the date to find fares for (ex. 11/25/2015)')
+  parser.add_argument('-s', '--start', help='the starting location (ex. Philadelphia)')
+  parser.add_argument('-e', '--end', help='the ending location (ex. Newark)')
 
-bus.set_outbound_date(leaving)
+  args = parser.parse_args()
 
-cities = bus.get_cities()
+  leaving = dparser.parse(args.date, fuzzy=True)
 
-start = random.randrange(len(cities))
-print "Start", cities[start][1]
+  print '== BOLTBUS ====================='
 
-dests = bus.set_start(start)
+  print '== Departure date:', leaving.strftime('%a %b %d, %Y')
 
-end = random.randrange(len(dests))
-print "End", dests[end][1]
+  Boltbus.set_outbound_date(leaving)
 
-fares = bus.set_dest(end)
+  cities = Boltbus.get_cities()
 
-print json.dumps(fares, indent=2)
+  startArr = [c for c in cities if args.start in c[1]]
+  if len(startArr) == 0:
+    citiesStr = ';\n\t'.join([', '.join(city) for city in cities])
+    raise ValueError('Start location \"' + args.start + '\" not found in:\n\t' + citiesStr)
+  startIndex = cities.index(startArr[0])
+  print '== Start:', cities[startIndex][1]
+
+  dests = Boltbus.set_start(startIndex)
+
+  endArr = [c for c in cities if args.end in c[1]]
+  if len(endArr) == 0:
+    destsStr = ';\n\t'.join([', '.join(dest) for dest in dests])
+    raise ValueError('End location \"' + args.end + '\" not found in:\n\t' + destsStr)
+  endIndex = dests.index(endArr[0])
+  print '== End:', dests[endIndex][1]
+
+  fares = Boltbus.set_dest(endIndex)
+  if len(fares) == 0:
+    print '==     No fares found for this date :('
+  else:
+    print json.dumps(fares, indent=2)
+
+  print '== end boltbus =================='
+
