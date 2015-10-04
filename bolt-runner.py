8a2e7b0a1336a143e97f2011a3c536a56b61d450
Alice <aliceren@seas.upenn.edu>
2015-10-04 00:04:38 -0400

    twilio integration - send results as text message

--- bolt-runner.py
+++ bolt-runner.py
@@ -1,24 +1,31 @@
-from boltbus import BoltbusScraper
 import datetime
 import json
 import argparse
 import dateutil.parser as dparser
+import os
+from boltbus import BoltbusScraper
+from twilio.rest import TwilioRestClient
 
+Twilio = TwilioRestClient()
 Boltbus = BoltbusScraper()
+
+twilio_dest = os.environ.get('MY_PHONE');
+twilio_src = os.environ.get('TWILIO_PHONE');
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-d', '--date', help='the date to find fares for (ex. 11/25/2015)', required=True)
   parser.add_argument('-s', '--start', help='the starting location (ex. Philadelphia)', required=True)
   parser.add_argument('-e', '--end', help='the ending location (ex. Newark)', required=True)
+  parser.add_argument('-t', '--text', action='store_true', help='if this flag is set, send results as a text message')
 
   args = parser.parse_args()
 
   leaving = dparser.parse(args.date, fuzzy=True)
 
   print '== BOLTBUS ====================='
-
-  print '== Departure date:', leaving.strftime('%a %b %d, %Y')
+  leavingStr = leaving.strftime('%a %b %d, %Y')
+  print '== Departure date: ' + leavingStr
 
   Boltbus.set_outbound_date(leaving)
 
@@ -42,9 +49,17 @@ if __name__ == '__main__':
 
   fares = Boltbus.set_dest(endIndex)
   if len(fares) == 0:
-    print '==     No fares found for this date :('
+    fares = 'None found :('
   else:
-    print json.dumps(fares, indent=2)
+    fares = json.dumps(fares, indent=2)
+  print 'Fares: ', fares
+
+  if args.text:
+    twilio_msg_header = 'Fares from ' + args.start + ' to ' + args.end + ' on ' + leavingStr + ': ' + fares
+    print '== Texting results to', twilio_dest
+    Twilio.messages.create(to=twilio_dest, from_=twilio_src,
+                           body=twilio_msg_header)
 
   print '== end boltbus =================='
+
 
